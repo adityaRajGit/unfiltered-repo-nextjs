e1eb530054bc9d42d48600e5016b2d90184ded9b
AdityaRajTaazaa <aditya.raj@taazaa.com>
2025-06-12 01:53:02 +0530

    m ch

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -4,6 +4,7 @@ export const validateToken = (token: string): boolean => {
     const payload = JSON.parse(atob(token.split('.')[1]));
     return !!payload.userId;
   } catch (error) {
+    console.log(error);
     return false;
   }
 };
