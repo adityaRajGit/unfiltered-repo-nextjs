1e84d1474872026519b479554171748814fdbf07
AdityaRajTaazaa <aditya.raj@taazaa.com>
2025-06-12 00:18:29 +0530

    token handling update

--- src/utils/decodeToken.ts
+++ src/utils/decodeToken.ts
@@ -1,17 +1,21 @@
 import { jwtDecode } from "jwt-decode";
 
 interface MyTokenPayload {
-    userId: {
-        name: string;
-        email: string;
-        _id: string;
-        phone: string;
-        username: string;
-        profile_pic: string;
-    };
-    iat: number;
-    role: string;
+  userId?: {
+    name: string;
+    email: string;
+    _id: string;
+    phone: string;
+    username: string;
+    profile_pic: string;
+  };
+  userIdObj?: {
+    userId: MyTokenPayload["userId"];
+  };
+  iat: number;
+  role: string;
 }
+
 
 
 export const decodeToken = (token: string): MyTokenPayload | null => {
@@ -19,7 +23,7 @@ export const decodeToken = (token: string): MyTokenPayload | null => {
     const decoded = jwtDecode<MyTokenPayload>(token);
     return {
       ...decoded,
-      userId: decoded.userId || (decoded as any).userIdObj?.userId,
+      userId: decoded.userId || decoded.userIdObj?.userId,
     };
   } catch (error) {
     console.error("Token decoding failed:", error);
